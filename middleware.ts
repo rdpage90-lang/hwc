9eea6513db1b38c9f5ebdcb04534a7c59b6838ce
rdpage90-lang <rdpage90@googlemail.com>
2026-09-23 13:04:54 +0100

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -2,13 +2,9 @@ import { auth } from "@/auth";
 import { NextResponse } from "next/server";
 
 export default auth((req) => {
-  if (!req.auth) {
-    return NextResponse.redirect(new URL("/login", req.url));
-  }
-
   return NextResponse.next();
 });
 
 export const config = {
-  matcher: ["/((?!login|_next/static|_next/image|favicon.ico).*)"],
+  matcher: ["/((?!_next/static|_next/image|favicon.ico).*)"],
 };
