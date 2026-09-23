b2a28e818031d653eb172b693872bdf15e1112e2
rdpage90-lang <rdpage90@googlemail.com>
2026-09-23 12:36:43 +0100

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -3,16 +3,8 @@ import { NextResponse } from "next/server";
 
 export default auth((req) => {
   console.log("PATH:", req.nextUrl.pathname);
+  console.log("ORIGIN:", req.nextUrl.origin);
   console.log("AUTH:", req.auth);
-
-  if (!req.auth) {
-    const loginUrl = new URL("/login", req.nextUrl.origin);
-    loginUrl.searchParams.set("callbackUrl", req.nextUrl.pathname);
-
-    console.log("REDIRECT:", loginUrl.toString());
-
-    return NextResponse.redirect(loginUrl);
-  }
 
   return NextResponse.next();
 });
